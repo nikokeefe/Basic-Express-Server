28546e0eaa3f93c492cecf860cee0d78c002dafe
Nik O'Keefe <nikwillok@gmail.com>
2019-09-30 12:23:51 +1300

    Frontend react blog build added

---
+++ src/build/precache-manifest.85f721a1d80ac717954ed3bed3e1794e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab7c329bb7e9a04a9855079c42068537",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0124033e6f215140be2",
+    "url": "/static/css/main.3cb707ef.chunk.css"
+  },
+  {
+    "revision": "e80cca8bb509fdca4754",
+    "url": "/static/js/2.6594c6e1.chunk.js"
+  },
+  {
+    "revision": "c0124033e6f215140be2",
+    "url": "/static/js/main.8528c0e6.chunk.js"
+  },
+  {
+    "revision": "afc0a052c644dce5e6bc",
+    "url": "/static/js/runtime-main.fbd335c9.js"
+  }
+]);
--- src/server.js
+++ src/server.js
@@ -1,15 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { MongoClient } from 'mongodb';
+import path from 'path';
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, '/build')));
 
 app.use(bodyParser.json());
 
 const withDB = async (operations, response) => {
 	try {
 		const client = await MongoClient.connect('mongodb://localhost:27017', {
-			useNewURLParser: true
+			useNewURLParser: true,
+			useUnifiedTopology: true
 		});
 		const db = client.db('my-blog');
 
@@ -80,6 +84,10 @@ app.post('/api/articles/:name/add-comment', (request, response) => {
 			.findOne({ name: articleName });
 		response.status(200).json(updatedArticleInfo);
 	}, response);
+});
+
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname + '/build/index.html'));
 });
 
 app.listen(8000, () => console.log('Listening on port 8000'));
